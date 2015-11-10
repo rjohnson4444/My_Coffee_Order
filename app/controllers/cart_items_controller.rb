8b6c39a865edef312c8c26ea2aea378d0b87f929
joshuajhun <joshuajhun@gmail.com>
2015-11-10 14:07:17 -0700

    still struggling with the can't find ID = 0 item '

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,6 +3,7 @@ class CartItemsController < ApplicationController
   def create
     item = Item.find(params[:id].to_i)
     @cart.add_item(item.id)
+
     session[:cart] = @cart.contents
     flash[:notice] = "You now have added #{item.title}."
     redirect_to items_path
