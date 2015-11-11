e8d94e896eb0672717ef01902f0af3308b3d1fb8
Ryan Johnson <rjohnson4444@live.com>
2015-11-10 21:42:46 -0700

    Add category model test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_attributes
+    @category = Category.new(name: "Coffee")
+  end
+
+  test "is valid" do
+    valid_attributes
+
+    assert @category.valid?
+  end
+
+  test "is invalid without a name" do
+    no_name_category = Category.new(name: "")
+
+    refute no_name_category.valid?
+  end
+
+  test "is associated with items" do
+    valid_attributes
+
+    assert_respond_to @category, :items
+  end
 end
