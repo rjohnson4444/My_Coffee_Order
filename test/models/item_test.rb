173ed4e6c24f2a268fa198aef9bf3f05d74591be
ZooBot Mars <sasa345@yahoo.com>
2015-11-11 09:01:39 -0700

    Item model testing almost done

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def valid_item
+    Item.new(title: "Hot Coffee", description: "ground heaven", price: 2, image: "http://i.dailymail.co.uk/i/pix/2013/10/24/article-0-16B39191000005DC-568_634x403.jpg")
+  end
+
+  test 'is valid' do
+    assert valid_item.valid?
+  end
+
+  test 'is invalid without a title description price or image' do
+    no_data_item = Item.new(title: "", description: "", price: "", image: "")
+
+    refute no_data_item.valid?
+  end
+
+
 end
