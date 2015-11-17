eceafd8c079c8f2868cf0d13d4246bfc9f537265
ZooBot Mars <sasa345@yahoo.com>
2015-11-17 14:24:01 -0700

    admin can view item dashboard

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,4 +4,13 @@ class Admin::ItemsController < Admin::BaseController
     @items = Item.all
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    # binding.pry
+    # if @item.update
+  end
+
 end
