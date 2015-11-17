b981fde32d39e87fc45a6cf38be775538bf778e5
ZooBot Mars <sasa345@yahoo.com>
2015-11-16 20:34:51 -0700

    admin dashboard redirect implemented

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,11 @@ class SessionController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:success] = "#{@user.username} has logged in"
-      redirect_to dashboard_path
+      if current_admin?
+        redirect_to admin_dashboard_index_path
+      else
+        redirect_to dashboard_path
+      end
     else
       flash.now[:error] = "Invalid Credentials. Try again."
       render :new
