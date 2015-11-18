f0134bfe9ec7908218b86b3da65d0c0cda7ddc52
ZooBot Mars <sasa345@yahoo.com>
2015-11-17 20:07:51 -0700

    Admin can edit an item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,8 +9,16 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def update
-    # binding.pry
-    # if @item.update
+    @item = Item.find(params[:id])
+    @item.update(title: params[:item][:title], description: params[:item][:description], price: params[:item][:price], image: params[:item][:image])
+    redirect_to admin_items_path
+    #NEEDTO Add validations and an else if an update doesn't save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image)
   end
 
 end
