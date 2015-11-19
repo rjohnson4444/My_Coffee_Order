3fc5b2c7989d0bb5d8f54187f4464a1c15515f98
ZooBot Mars <sasa345@yahoo.com>
2015-11-18 18:08:12 -0700

    Admin can create an item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,19 @@
 class Admin::ItemsController < Admin::BaseController
 
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      flash[:errors] = "Missing fields. Please try again."
+      redirect_to new_admin_item_path 
+    end
+  end
+
   def index
     @items = Item.all
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  validates :title, :description, :price, :image, presence: true
+  validates :title, :description, :price , :image, presence: true
+  validates :title, uniqueness: true
   has_many :order_items
   has_many :orders, through: :order_items
-
-  # #first way
-  # def to_param
-  #   binding.pr
-  #   "#{id} #{title}".parameterize
-  # end
-  # second way
-  # extend FriendlyId
-  # friendly_id :title
-
-  # # third way with slug
-  # extend FriendlyId
-  # friendly_id :title, use: :slugged
 
 end
