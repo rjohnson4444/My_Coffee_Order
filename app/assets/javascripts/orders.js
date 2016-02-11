d81ed9313615d799773bc6a5c82f25ed38354e00
Ryan Johnson <rjohnson4444@live.com>
2016-02-10 22:33:14 -0700

    edit readme

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -4,55 +4,55 @@ $(document).ready(function(){
 
 
   $('#status-ordered').on('click', function () {
-  $status = $('.order')
-  var currentStatus = 'ordered'
-  $status.each(function (index, order) {
-    $status = $(order);
-    if ($status.data('status') === currentStatus) {
-      $status.show();
-    } else {
-      $status.hide();
-    }
+    $status = $('.order')
+    var currentStatus = 'ordered'
+    $status.each(function (index, order) {
+      $status = $(order);
+      if ($status.data('status') === currentStatus) {
+        $status.show();
+      } else {
+        $status.hide();
+      }
     });
   });
 
 
   $('#status-paid').on('click', function () {
-  $status = $('.order')
-  var currentStatus = 'paid'
-  $status.each(function (index, status) {
-    $status = $(status);
-    if ($status.data('status') === currentStatus) {
-      $status.show();
-    } else {
-      $status.hide();
-    }
+    $status = $('.order')
+    var currentStatus = 'paid'
+    $status.each(function (index, status) {
+      $status = $(status);
+      if ($status.data('status') === currentStatus) {
+        $status.show();
+      } else {
+        $status.hide();
+      }
     });
   });
 
   $('#status-cancelled').on('click', function () {
-  $status = $('.order')
-  var currentStatus = 'cancelled'
-  $status.each(function (index, status) {
-    $status = $(status);
-    if ($status.data('status') === currentStatus) {
-      $status.show();
-    } else {
-      $status.hide();
-    }
+    $status = $('.order')
+    var currentStatus = 'cancelled'
+    $status.each(function (index, status) {
+      $status = $(status);
+      if ($status.data('status') === currentStatus) {
+        $status.show();
+      } else {
+        $status.hide();
+      }
     });
   });
 
   $('#status-completed').on('click', function () {
-  $status = $('.order')
-  var currentStatus = 'completed'
-  $status.each(function (index, status) {
-    $status = $(status);
-    if ($status.data('status') === currentStatus) {
-      $status.show();
-    } else {
-      $status.hide();
-    }
+    $status = $('.order')
+    var currentStatus = 'completed'
+    $status.each(function (index, status) {
+      $status = $(status);
+      if ($status.data('status') === currentStatus) {
+        $status.show();
+      } else {
+        $status.hide();
+      }
+      });
     });
   });
-});
